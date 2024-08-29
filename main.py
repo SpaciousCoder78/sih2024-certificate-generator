0594398d0323f11fa8dbaa79df2df16b016ef940
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-08-29 06:43:58 +0530

    finished the base

--- main.py
+++ main.py
@@ -2,17 +2,27 @@
 #developed by : Aryan Karamtoth of Department of Information Technology, KITS Warangal
 
 #importing modules
+# importing modules
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
 
-# persons= pd.read_csv('persons.csv')
+# read the csv file
+persons = pd.read_csv('persons.csv')
 
-namelist = "name"
-im=Image.open("sample.jpg")
-draw = ImageDraw.Draw(im)
-location = (100, 100)
-text_color = (0, 188, 255)
-selectFont = ImageFont.truetype("arial.ttf", size=45)
-draw.text(location, namelist, fill = text_color, font = selectFont)
-im.save("certificate.jpg")
+# find all columns that contain "Student Full Name" in their name
+name_columns = [col for col in persons.columns if "Student Full Name" in col]
 
+# iterate over the name columns
+for name_column in name_columns:
+    # convert the column to a list
+    namelist = persons[name_column].tolist()
+
+    # iterate over the names in the list
+    for i in namelist:
+        im = Image.open("sample.jpg")
+        draw = ImageDraw.Draw(im)
+        location = (240, 311)
+        text_color = (0, 0, 0)
+        selectFont = ImageFont.truetype("arial.ttf", size=45)
+        draw.text(location, i, fill = text_color, font = selectFont)
+        im.save(f"certificates\\certificate_{i}.jpg")
