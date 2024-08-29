ef0c7afe52abc8e342b3a464a9f980d1b043b0dd
Aryan Karamtoth <88923986+SpaciousCoder78@users.noreply.github.com>
2024-08-29 13:29:06 +0530

    got it working

--- main.py
+++ main.py
@@ -3,8 +3,15 @@
 
 #importing modules
 # importing modules
+# importing modules
+# importing modules
+# importing modules
+# importing modules
+# importing modules
+# importing modules
 import pandas as pd
 from PIL import Image, ImageDraw, ImageFont
+import textwrap
 
 # read the csv file
 persons = pd.read_csv('persons.csv')
@@ -21,8 +28,20 @@ for name_column in name_columns:
     for i in namelist:
         im = Image.open("sample.jpg")
         draw = ImageDraw.Draw(im)
-        location = (463, 513)
+        location = (223, 423)
         text_color = (0, 0, 0)
-        selectFont = ImageFont.truetype("arial.ttf", size=45)
-        draw.text(location, i, fill = text_color, font = selectFont)
+        selectFont = ImageFont.truetype("OpenSans-Regular.ttf", size=25)  # use Open Sans Regular font
+
+        # format the text to be printed on the image
+        text = f"This is to certify that {i} successfully participated in the Internal Hackathon for Smart India Hackathon 2024 at KITSW held on 31/08/2024"
+
+        # wrap the text into multiple lines if it's too long
+        wrapper = textwrap.TextWrapper(width=80)  # decrease width to increase line breaks
+        text_lines = wrapper.wrap(text=text)
+
+        # draw each line of text
+        for line in text_lines:
+            draw.text(location, line, fill=text_color, font=selectFont)
+            location = (location[0], location[1] + 35)  # increase vertical spacing between lines
+
         im.save(f"certificates\\certificate_{i}.jpg")
